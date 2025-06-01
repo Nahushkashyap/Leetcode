d4d5194bafbe82c91c8cfa229ec7c69a86a7be7a
Nahush <nahush@JAR-LT-033.local>
2025-06-01 18:52:33 +0530

    solved leetcode 1218

---
+++ 1218. Longest Arthemetic Subsequence of Given Difference.py
@@ -0,0 +1,15 @@
+#Question :- https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        if (len(arr) == 0):
+            return 0
+        dp = {}
+        ans = 1
+        for i in arr:
+            if (i - difference in dp):
+                dp[i] = dp[i - difference] + 1
+                ans = max(ans, dp[i])
+                continue
+            dp[i] = 1
+
+        return ans
