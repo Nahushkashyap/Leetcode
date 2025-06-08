e48072d2d6c4159952fd8704434c6c580cf24552
Nahush <nahush@JAR-LT-033.local>
2025-06-08 16:23:11 +0530

    solved Relocate Marbles question

---
+++ 2766. Relocate Marbles.py
@@ -0,0 +1,14 @@
+#Question :- https://leetcode.com/problems/relocate-marbles/
+class Solution:
+    def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
+        nums = set(nums)
+        for i in range(len(moveFrom)):
+            moveToPosition = moveTo[i]
+            moveFromPosition = moveFrom[i]
+            if (moveToPosition == moveFromPosition):
+                continue
+            nums.add(moveToPosition)
+            nums.remove(moveFromPosition)
+        nums = list(nums)
+        nums.sort()
+        return nums
