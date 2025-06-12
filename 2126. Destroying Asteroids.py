f71d790a7c5300a6c2056c9cbe384deb1c9eb356
Nahush <manahushkashyap6@icloud.com>
2025-06-13 03:01:51 +0530

    solved destroying asteroids

---
+++ 2126. Destroying Asteroids.py
@@ -0,0 +1,9 @@
+#Question :- https://leetcode.com/problems/destroying-asteroids/
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for i in asteroids:
+            if(mass < i):
+                return False
+            mass+=i
+        return True
