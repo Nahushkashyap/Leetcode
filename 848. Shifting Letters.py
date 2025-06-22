89d07559455c07e2dd900bb806eaf5f84965895a
Nahush Kashyap <nahushkashyap@Nahushs-MacBook-Pro.local>
2025-06-22 23:45:31 +0530

    solved shifting letter

---
+++ 848. Shifting Letters.py
@@ -0,0 +1,14 @@
+#Question :- https://leetcode.com/problems/shifting-letters/
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        s = list(s)
+        shiftVal = 0
+        for i in range(1, len(s) + 1):
+            startingNum = ord(s[-i]) - 96
+            shiftVal += shifts[-i]
+            sumVal = shiftVal + startingNum
+            sumVal %= 26
+            if (sumVal == 0):
+                sumVal = 26
+            s[-i] = chr(sumVal + 96)
+        return ''.join(s)
